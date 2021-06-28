0c13a3f9f6043d8bd9ba682c70a410fd0866cd91
RezwanJamee <rezwan.jamee@gmail.com>
2021-06-28 23:18:03 +1000

    Auth API implemented.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+    })
+    .then(console.log("MongoDB Connected."))
+    .catch((err) => console.log(err));
+
+    app.use("/api/auth", authRoute);
+
+app.listen("5000", () => {
+    console.log("Backend is running...");
+});
