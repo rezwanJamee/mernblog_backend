60f1f736f7fb6cd0851ade84e12f223654f78e3e
RezwanJamee <rezwan.jamee@gmail.com>
2021-06-28 23:43:21 +1000

    User API implemented.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+//Routes
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
 
 dotenv.config();
 app.use(express.json());
@@ -15,7 +19,9 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(console.log("MongoDB Connected."))
     .catch((err) => console.log(err));
 
-    app.use("/api/auth", authRoute);
+mongoose.set('useFindAndModify', false);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running...");
